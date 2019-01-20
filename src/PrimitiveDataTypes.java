b85a217dd572446afe1fb4fca183fc42bd4fd761
jixin <lichanping@hotmail.com>
2019-01-20 11:50:46 +0800

    first commit

---
+++ src/PrimitiveDataTypes.java
@@ -0,0 +1,8 @@
+public class PrimitiveDataTypes {
+    public static void main(String[] args){
+        boolean flag = true;
+        byte range = 124;
+        System.out.println(flag);
+        System.out.println(range);
+    }
+}
